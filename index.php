9d91efbbcef7a44e749bce67e450415843661ebc
alicenyberg <alice.nyberg@hotmail.com>
2021-10-18 10:11:16 +0200

    added file structure

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?= 'The Elephpent in the Room'; ?>
+<?php require __DIR__ . '/header.php'; ?>
+
+<?php require __DIR__ . '/functions.php'; ?>
+
+<header>
+    <h1>Hello!<h1>
+</header>
+
+<?php require __DIR__ . '/footer.php'; ?>
