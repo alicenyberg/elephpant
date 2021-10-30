b07356337fd94ace9f07e5774e83f2598b1a17f6
alicenyberg <alice.nyberg@hotmail.com>
2021-10-31 01:18:00 +0200

    added more css and functions

--- functions.php
+++ functions.php
@@ -7,6 +7,19 @@ $date = date("d-m-Y");
 
 function madeBy($firstName, $lastName)
 {
-    return "Made by $firstName $lastName";
+    return "$firstName $lastName";
 }
 $author = madeBy('Alice', 'Nyberg');
+
+
+
+
+$startDate = date("Y-m-d");
+$endDate = "2022-01-06";
+$daysLeft = daysLeft($startDate, $endDate);
+
+function daysLeft($startDate, $endDate)
+{
+    $days = strtotime($startDate) - strtotime($endDate);
+    return abs($days / 86400);
+}
--- index.php
+++ index.php
@@ -1,10 +1,33 @@
 <?php require __DIR__ . '/header.php'; ?>
 <?php require __DIR__ . '/data.php'; ?>
-
-<?php require __DIR__ . '/content.php'; ?>
-
-
-
 <?php require __DIR__ . '/functions.php'; ?>
+
+<main>
+
+    <h1>This is a website with cute dogs!</h1>
+
+    <div class="dogs_wrapper">
+
+        <?php foreach ($dogs as $dog) : ?>
+            <div class="allDogs">
+                <h2><?php echo $dog['name']; ?></h2>
+                <img src="<?php echo $dog['image']; ?>" alt="<?php echo $dog['name']; ?>">
+                <p><?php echo $dog['info']; ?></p>
+            </div>
+        <?php endforeach; ?>
+    </div>
+
+    <div class="facts_wrapper">
+        <ul class="facts">
+            <h3>Fun facts about dogs!</h3>
+            <?php foreach ($funFacts as $funFact) : ?>
+                <li> <?php echo $funFact; ?></li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+
+    <?php echo "Today it's $daysLeft days left until My Dog" ?>
+
+</main>
 
 <?php require __DIR__ . '/footer.php'; ?>
